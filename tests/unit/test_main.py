630918bab0f4a92bbd2cfeb3f405e6b806b9f475
enuno <e.np99personal@gmail.com>
2026-04-28 15:38:10 +0200

    feat!(querybuilder): correct query builder function is dispatched based on pydantic model

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -2,83 +2,97 @@
 
 import src.constants as constants
 from src.main import (
-    build_top_words_query,
-    build_specific_word_query,
+    DateRangeParams,
+    TopWordsParams,
+    WordFreqParams,
+    build_query,
+    get_top_word_query_type,
+    get_word_freq_query_type,
     is_within_preprocessed_range,
 )
 from tests.constants import SAMPLE_PROCESSED_WORD, SAMPLE_UNPROCESSED_WORD
+
 
 # --- is_within_preprocessed_range ---
 
 
 def test_within_preprocessed_range():
     assert is_within_preprocessed_range(
-        constants.PROCESSED_DATA_START_YEAR, constants.PROCESSED_DATA_END_YEAR
+        DateRangeParams(
+            constants.PROCESSED_DATA_START_YEAR, constants.PROCESSED_DATA_END_YEAR
+        )
     )
 
 
 def test_outside_preprocessed_range():
     assert not is_within_preprocessed_range(
-        constants.RAW_DATA_START_YEAR, constants.RAW_DATA_END_YEAR
+        DateRangeParams(constants.RAW_DATA_START_YEAR, constants.RAW_DATA_END_YEAR)
     )
 
 
 def test_start_year_before_preprocessed_range():
     assert not is_within_preprocessed_range(
-        constants.RAW_DATA_START_YEAR, constants.PROCESSED_DATA_END_YEAR
+        DateRangeParams(
+            constants.RAW_DATA_START_YEAR, constants.PROCESSED_DATA_END_YEAR
+        )
     )
 
 
-# --- build_query ---
+# --- build_query (top words) ---
 
 
-def test_build_query_uses_preprocessed_table():
-    sql = build_top_words_query(
-        constants.PROCESSED_DATA_START_YEAR,
-        constants.PROCESSED_DATA_END_YEAR,
-        constants.TOP_WORDS_DEFAULT_LIMIT,
+def test_build_top_words_query_uses_preprocessed_table():
+    params = TopWordsParams(
+        start_year=constants.PROCESSED_DATA_START_YEAR,
+        end_year=constants.PROCESSED_DATA_END_YEAR,
+        word_limit=constants.TOP_WORDS_DEFAULT_LIMIT,
     )
+    sql = build_query(get_top_word_query_type(params))
     assert constants.PREPROCESSED_TABLE_NAME in sql
     assert constants.UNPROCESSED_TABLE_NAME not in sql
 
 
-def test_build_query_uses_unprocessed_table():
-    sql = build_top_words_query(
-        constants.RAW_DATA_START_YEAR,
-        constants.RAW_DATA_END_YEAR,
-        constants.TOP_WORDS_DEFAULT_LIMIT,
+def test_build_top_words_query_uses_unprocessed_table():
+    params = TopWordsParams(
+        start_year=constants.RAW_DATA_START_YEAR,
+        end_year=constants.RAW_DATA_END_YEAR,
+        word_limit=constants.TOP_WORDS_DEFAULT_LIMIT,
     )
+    sql = build_query(get_top_word_query_type(params))
     assert constants.UNPROCESSED_TABLE_NAME in sql
     assert constants.PREPROCESSED_TABLE_NAME not in sql
 
 
-# --- build_specific_word_query ---
+# --- build_query (word freq) ---
 
 
-def test_build_specific_word_query_uses_preprocessed_table():
-    sql = build_specific_word_query(
-        constants.PROCESSED_DATA_START_YEAR,
-        constants.PROCESSED_DATA_END_YEAR,
-        SAMPLE_PROCESSED_WORD,
+def test_build_word_freq_query_uses_preprocessed_table():
+    params = WordFreqParams(
+        start_year=constants.PROCESSED_DATA_START_YEAR,
+        end_year=constants.PROCESSED_DATA_END_YEAR,
+        word=SAMPLE_PROCESSED_WORD,
     )
+    sql = build_query(get_word_freq_query_type(params))
     assert constants.PREPROCESSED_TABLE_NAME in sql
     assert constants.UNPROCESSED_TABLE_NAME not in sql
 
 
-def test_build_specific_word_query_uses_unprocessed_table():
-    sql = build_specific_word_query(
-        constants.RAW_DATA_START_YEAR,
-        constants.RAW_DATA_END_YEAR,
-        SAMPLE_UNPROCESSED_WORD,
+def test_build_word_freq_query_uses_unprocessed_table():
+    params = WordFreqParams(
+        start_year=constants.RAW_DATA_START_YEAR,
+        end_year=constants.RAW_DATA_END_YEAR,
+        word=SAMPLE_UNPROCESSED_WORD,
     )
+    sql = build_query(get_word_freq_query_type(params))
     assert constants.UNPROCESSED_TABLE_NAME in sql
     assert constants.PREPROCESSED_TABLE_NAME not in sql
 
 
-def test_build_specific_word_query_includes_word():
-    sql = build_specific_word_query(
-        constants.PROCESSED_DATA_START_YEAR,
-        constants.PROCESSED_DATA_END_YEAR,
-        SAMPLE_PROCESSED_WORD,
+def test_build_word_freq_query_includes_word():
+    params = WordFreqParams(
+        start_year=constants.PROCESSED_DATA_START_YEAR,
+        end_year=constants.PROCESSED_DATA_END_YEAR,
+        word=SAMPLE_PROCESSED_WORD,
     )
+    sql = build_query(get_word_freq_query_type(params))
     assert SAMPLE_PROCESSED_WORD in sql
